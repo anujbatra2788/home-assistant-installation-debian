509cbdae7b43cbde3b9f64475839082cd02dc11c
anujbatra2788 <anujbatra2788@gmail.com>
2023-01-08 12:02:48 +1100

    Adding installation of zigbee2mqtt
    Config and setup for mosquitto and zigbee2mqtt

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -29,7 +29,14 @@ sudo apt install -y vim \
 	libreadline-dev \
 	libffi-dev \
 	libsqlite3-dev \
-	libbz2-dev
+	libbz2-dev \
+	mosquitto \
+	mosquitto-clients \
+	nodejs \
+	git \
+	make \
+	g++ \
+	gcc
 
 wget https://www.python.org/ftp/python/3.10.9/Python-3.10.9.tgz
 tar -xf Python-3.10.*.tgz
@@ -55,3 +62,32 @@ fi
 echo "Installing OS Agent"
 wget -O os-agent.deb https://github.com/home-assistant/os-agent/releases/download/1.4.1/os-agent_1.4.1_linux_x86_64.deb
 sudo dpkg -i os-agent.deb
+
+echo "Setting config for mosquitto"
+echo '' | sudo tee -a /etc/mosquitto/mosquitto.conf
+echo 'listener 1883' | sudo tee -a /etc/mosquitto/mosquitto.conf
+echo 'allow_anonymous true' | sudo tee -a /etc/mosquitto/mosquitto.conf
+sudo sed  -i '5i per_listener_settings true' /etc/mosquitto/mosquitto.conf
+
+sudo systemctl restart mosquitto
+
+echo "Setting config for zigbee2mqtt"
+sudo curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo mkdir /opt/zigbee2mqtt
+sudo chown -R ${USER}: /opt/zigbee2mqtt
+git clone --depth 1 https://github.com/Koenkk/zigbee2mqtt.git /opt/zigbee2mqtt
+
+# Install dependencies (as user)
+cd /opt/zigbee2mqtt
+npm ci
+
+echo "Changing configuration for zigbee2mqtt"
+echo 'frontend: true' | tee -a /opt/zigbee2mqtt/data/configuration.yaml
+echo '' | tee -a /opt/zigbee2mqtt/data/configuration.yaml
+echo '' | tee -a /opt/zigbee2mqtt/data/configuration.yaml
+
+echo 'advanced:' | tee -a /opt/zigbee2mqtt/data/configuration.yaml
+echo '  network_key: GENERATE' | tee -a /opt/zigbee2mqtt/data/configuration.yaml
+
+sudo mv ./zigbee2mqtt.service /etc/systemd/system/zigbee2mqtt.service
+
